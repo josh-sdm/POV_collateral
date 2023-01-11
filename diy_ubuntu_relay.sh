09f6d34b67ae07f464a012882d957501e963055d
Josh Hoge <95385244+josh-sdm@users.noreply.github.com>
2023-01-11 14:58:02 -0500

    Update diy_ubuntu_relay.sh

--- diy_ubuntu_relay.sh
+++ diy_ubuntu_relay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This script should work for VM user data as well as running VMs where you wish to install a strongDM gateway
-# Your VM should have a public IPv4 address and a firewall rule that allows 5000/tcp from 0.0.0.0/0
+# This script should work for VM user data as well as running VMs where you wish to install a strongDM relay
+# Your VM should have an egress route and firewall rules that allow it to reach your strongDM gateways 5000/tcp 
 # Your VM should also have a friendly name that contains only digits, lowercase characters, and/or - (dash/tack)
 #
 # Create SDM_ADMIN_TOKEN in the strongDM Admin UI with the four scopes in the line below
@@ -18,7 +18,7 @@ apt install jq unzip -y
 useradd -m strongdm
 usermod -aG sudo strongdm
 
-# Download the strongDM gateway binary and unzip it
+# Download the strongDM binary and unzip it
 curl -J -O -L https://app.strongdm.com/releases/cli/linux
 unzip sdmcli* && rm -f sdmcli*
 
@@ -28,16 +28,16 @@ export INSTANCE_PRIVATE_IP=<YOUR_INSTANCE_PRIVATE_IP>
 export INSTANCE_ID=<YOUR_INSTANCE_UNIQUE_ID>
 export INSTANCE_FRIENDLY_NAME=<YOUR_INSTANCE_FRIENDLY_NAME>
 
-# Use the sdm binary with SDM_ADMIN_TOKEN to create a ssh certificate authority resource for the gateway
+# Use the sdm binary with SDM_ADMIN_TOKEN to create a ssh certificate authority resource for the relay
 ./sdm admin servers create ssh-cert --hostname $INSTANCE_PRIVATE_IP --port 22 --username $SSH_USERNAME $INSTANCE_FRIENDLY_NAME-ssh --tags instance=$INSTANCE_ID
 
-# Set SDM_RELAY_TOKEN token by using the sdm binary with SDM_ADMIN_TOKEN to add a new gateway
+# Set SDM_RELAY_TOKEN token by using the sdm binary with SDM_ADMIN_TOKEN to add a new relay
 export SDM_RELAY_TOKEN=`./sdm admin relays create --name $INSTANCE_FRIENDLY_NAME --tags instance=$INSTANCE_ID`
 export SDM_CA_PUB=`./sdm admin ssh view-ca`
 unset SDM_ADMIN_TOKEN
 ./sdm install --relay --user strongdm
 
-# Add the public certificate from your strongDM tenant to the sshd config of the gateway
+# Add the public certificate from your strongDM tenant to the sshd config of the relay
 echo $SDM_CA_PUB | tee -a /etc/ssh/sdm_ca.pub
 echo "TrustedUserCAKeys /etc/ssh/sdm_ca.pub" | tee -a /etc/ssh/sshd_config
 systemctl restart ssh
